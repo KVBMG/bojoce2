2eff9b1b231f9f87c0a92832a8cc2433b73d4048
ValeryNomenjanahary <ValeryNomenjanahary@fidiarilantov@yahoo.com>
2017-11-28 22:28:45 +0300

    Assetic

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -29,6 +29,7 @@ class AppKernel extends Kernel
             new EcoJob\MessagerieBundle\EcoJobMessagerieBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new EcoJob\AnonymousBundle\EcoJobAnonymousBundle(),
+                        new Symfony\Bundle\AsseticBundle\AsseticBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
