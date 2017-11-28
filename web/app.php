cc3e5ee1782daf34f9f73abb245d2b05f22f770e
ValeryNomenjanahary <ValeryNomenjanahary@fidiarilantov@yahoo.com>
2017-11-28 10:52:07 +0300

    Com

--- web/app.php
+++ web/app.php
@@ -2,6 +2,9 @@
 
 use Symfony\Component\HttpFoundation\Request;
 
+require_once __DIR__ . '/../app/bootstrap.php.cache';
+require_once __DIR__ . '/../app/AppKernel.php';
+
 /** @var \Composer\Autoload\ClassLoader $loader */
 $loader = require __DIR__.'/../app/autoload.php';
 include_once __DIR__.'/../app/bootstrap.php.cache';
